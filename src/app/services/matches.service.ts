b049444018515a100ccb28c1aa8362c488af5788
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-05-10 21:49:38 +0100

    getMatches

--- src/app/services/matches.service.ts
+++ src/app/services/matches.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchesService {
 
-  constructor() { }
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = `${environment.apiUrl}competitions/`;
+  }
+
+  getMatches(competition: string) {
+    return this.http.get(`${this.url}${competition}/matches`);
+  }
 }
