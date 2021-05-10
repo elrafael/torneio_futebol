3f0fbee72d0d8584c30c41bee6ed4a4b010c6707
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-05-10 19:50:20 +0100

    httpclient and headers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompetitionsComponent } from './competitions/competitions.component';
+import { AddHeaderInterceptor } from './http-interceptors/addHeaderInterceptor ';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetitionsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AddHeaderInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
