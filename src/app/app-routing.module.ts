2afa30de282afaebf0b02dea0d43432d6a159840
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-05-11 14:04:43 +0100

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CompetitionsComponent } from './competitions/competitions.component';
-import { FixturesComponent } from './fixtures/fixtures.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { MatchesComponent } from './matches/matches.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-  { path: 'homepage', component: HomepageComponent },
-  { path: 'matches', component: FixturesComponent },
-  { path: 'competitions', component: CompetitionsComponent },
-  { path: 'matches/:competition', component: MatchesComponent },
+  { path: 'homepage', loadChildren: () => import('./homepage/homepage.module').then(m => m.HomepageModule) },
+  { path: 'competitions', loadChildren: () => import('./competitions/competitions.module').then(m => m.CompetitionsModule) },
+  { path: 'matches/:competition', loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule) },
+  { path: 'teams', loadChildren: () => import('./teams/teams.module').then(m => m.TeamsModule) },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CompetitionsComponent } from './competitions/competitions.component';
 import { AddHeaderInterceptor } from './http-interceptors/addHeaderInterceptor ';
-import { MatchesComponent } from './matches/matches.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CompetitionsComponent,
-    MatchesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
