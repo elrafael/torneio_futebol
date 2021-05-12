bfc27c509e824ca670c7bca1ed8bcf04ef0c9f3a
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-05-12 09:17:59 +0100

    testing broken

--- src/app/services/competitions.service.spec.ts
+++ src/app/services/competitions.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CompetitionsService } from './competitions.service';
+import { Competitions } from '../interfaces/competitions';
 
 describe('CompetitionsService', () => {
   let service: CompetitionsService;
@@ -8,6 +9,13 @@ describe('CompetitionsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CompetitionsService);
+  });
+
+  it('#getByCompetition should return an Observable', (done: DoneFn) => {
+    service.getByCompetition('PL').subscribe( value => {
+      expect(value).toBe('observable value');
+      done();
+    })
   });
 
   it('should be created', () => {
