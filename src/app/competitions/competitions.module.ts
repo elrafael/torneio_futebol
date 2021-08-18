16fc6512019fb69bba72e959b782394e127730df
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-08-18 12:21:52 +0100

    detail module inside competitions

--- src/app/competitions/competitions.module.ts
+++ src/app/competitions/competitions.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CompetitionsRoutingModule } from './competitions-routing.module';
 import { CompetitionsComponent } from './competitions.component';
+import { DetailCompetitionComponent } from './detail-competition/detail-competition.component';
+import { MatListModule } from '@angular/material/list';
+import { MaterialModule } from '../shared/material.module';
 
 
 @NgModule({
   declarations: [
     CompetitionsComponent,
+    DetailCompetitionComponent,
   ],
   imports: [
     CommonModule,
-    CompetitionsRoutingModule
+    CompetitionsRoutingModule,
+    MaterialModule
   ]
 })
 export class CompetitionsModule {}
