17fd1a70e5bff27c429728444ff1733825485aa5
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-08-18 18:53:56 +0100

    list competition route

--- src/app/competitions/competitions-routing.module.ts
+++ src/app/competitions/competitions-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompetitionsComponent } from './competitions.component';
+import { ListCompetitionsComponent } from './list-competitions/list-competitions.component';
 import { StandingsComponent } from './standings/standings.component';
 
 const routes: Routes = [
   { path: '', component: CompetitionsComponent },
+  { path: 'list', component: ListCompetitionsComponent },
   { path: 'standings/:id', component: StandingsComponent },
 ];
 
