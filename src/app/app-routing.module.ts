a93691b265b9519635201fe096b75e97aa8cfc55
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-08-18 12:19:58 +0100

    standing route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'competitions', loadChildren: () => import('./competitions/competitions.module').then(m => m.CompetitionsModule) }];
+const routes: Routes = [{ path: 'competitions', loadChildren: () => import('./competitions/competitions.module').then(m => m.CompetitionsModule) }, { path: 'standings', loadChildren: () => import('./standings/standings.module').then(m => m.StandingsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
