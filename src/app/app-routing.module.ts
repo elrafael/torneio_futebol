df51ff791fc8cb0b63102960a12bbaed2ca71ec4
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-12-01 11:46:09 +0000

    routing to teams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: 'competitions', loadChildren: () => import('./competitions/competitions.module').then(m => m.CompetitionsModule) }
+  { path: 'competitions', loadChildren: () => import('./competitions/competitions.module').then(m => m.CompetitionsModule) },
+  { path: 'teams', loadChildren: () => import('./teams/teams.module').then(m => m.TeamsModule) },
 ];
 
 @NgModule({
